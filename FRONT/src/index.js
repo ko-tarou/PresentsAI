b35d7f562070f4436cc1d0e7b46ef3cf72c4d94b
hry-923 <c1419395@st.kanazawa-it.ac.jp>
2024-11-30 18:04:52 +0900

    最終確認

--- FRONT/src/index.js
+++ FRONT/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css'; // スタイル用（存在しない場合は削除してもOK）
+// import './index.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
