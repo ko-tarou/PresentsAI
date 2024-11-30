16905bd0c1d635d31dd9203365450ff607b091aa
mei <c1419786@st.kanazawa-it.ac.jp>
2024-11-30 20:41:05 +0900

    作ったけど表示されません

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -1,12 +1,48 @@
+import "./App.css"
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
+import SlideView from "./pages/SlideView/SlideView.js";
+import React, { useState } from "react";
+
+const Header = () => {
+    const location = useLocation();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+  
+    return (
+      <header>
+        <nav>
+          <Link to="/">ホーム</Link>
+          <Link to="/project">プロジェクト</Link>
+          <Link to="/slide">スライド</Link>
+          <Link to="/test">実験</Link>
+          {['/', '/slide'].includes(location.pathname) && (
+            <div className="search-container">
+              <input
+                type="text"
+                placeholder="検索..."
+                className="search-field"
+              />
+              <button className="search-button">検索</button>
+            </div>
+          )}
+        </nav>
+        <button className="circle-button" onClick={openModal}>?</button>
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
+      </header>
+    );
+  };
+
 function App() {
     return (
-    <div className="App">
-        <header className="App-header">
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-        </header>
-    </div>
+        <Router>
+            <Header />
+             <Routes>
+                <Route path="/" element={<Toppage />} />
+                <Route path="/slideview" element={<SlideView />} />
+            </Routes>
+        </Router>
     );
 }
 export default App;
---
+++ FRONT/src/pages/SlideView/SlideView.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./SlideView.css"
+
+const SlideView = () => {
+  return (
+    <div className='SlideView'>
+        <div className='leftside'>
+          左側
+        </div>
+        <div className='rightside'>
+          <div className='box-whole'>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+            <div className='box'></div>
+          </div>    
+        </div>
+
+
+    </div>
+  )
+}
+
+export default SlideView
