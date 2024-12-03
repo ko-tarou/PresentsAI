dc3aaef1ad29c0bff705c8c1b0f5ce6b690db3f4
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-12-04 06:52:38 +0900

    いったんコタのやつパクったよ

---
+++ FRONT/src/components/SlidePage/ShapeBox/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextBox from './ShapeBox';
+
+function ShapeBoxArea() {
+	return (
+    <div style={{ padding: '10px', borderRight: '1px solid gray' }}>
+      <TextBox text="T" />
+    </div>
+  );
+}
+
+export default ShapeBoxArea;
--- FRONT/src/pages/SlideView/SlideView.js
+++ FRONT/src/pages/SlideView/SlideView.js
@@ -3,28 +3,28 @@ import "./SlideView.css"
 
 
 const SlideView = () => {
-  return (
-    <div className='SlideView'>
-        <div className='leftside'>
-          左側
-        </div>
-        <div className='rightside'>
-          <div className='box-whole'>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-            <div className='box'></div>
-          </div>    
-        </div>
+return (
+	<div className='SlideView'>
+		<div className='leftside'>
+		左側
+		</div>
+		<div className='rightside'>
+		<div className='box-whole'>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+			<div className='box'></div>
+		</div>    
+		</div>
 
 
-    </div>
-  )
+	</div>
+)
 }
 
 export default SlideView
