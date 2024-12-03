25643e019c5735172833914872cfba86504672a0
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-12-03 21:46:30 +0900

    SlideViewを改造するよ！！！

---
+++ FRONT/src/components/TopPage/ChangeQuantity/ChangeQuantity.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import './ChangeQuantity.css'
+
+export default function ChangeQuantity() {
+    
+    const Columns = () => {
+      const [columns, setColumns] = useState(3); 
+    };
+}    
+    
