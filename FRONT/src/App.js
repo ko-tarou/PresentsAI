29242364b011eb5319e45e750c5126d128883735
ko-tarou <akokoa1221@gmail.com>
2024-12-03 12:13:30 +0900

    ドラッグ機能追加

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import TopPage from './pages/TopPage/TopPage.js';
 import SlideView from './pages/SlideView/SlideView.js';
 import SlidePage from './pages/SlidePage/SlidePage.js';
-
 import PresenPage from './pages/PresenPage/PresenPage.js';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<TopPage />} />
         <Route path="/slideview" element={<SlideView />} />
         <Route path="/slidepage" element={<SlidePage />} />
-        <Route path="/presen" element={<PresenPage/>}/>
+        <Route path="/presen" element={<PresenPage />} />
       </Routes>
     </Router>
   );
---
+++ FRONT/src/components/SlidePage/TextBox/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextBox from './TextBox';
+
+function TextBoxArea() {
+	return (
+    <div style={{ padding: '10px', borderRight: '1px solid gray' }}>
+      <TextBox text="T" />
+    </div>
+  );
+}
+
+export default TextBoxArea;
