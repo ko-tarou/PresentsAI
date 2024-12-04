ca9d1e240dfa79a68b17a585f02d4cbe80b393e4
karir <c1406274@st.kanazawa-it.ac.jp>
2024-12-04 14:07:52 +0900

    HintButtonできた

---
+++ FRONT/src/components/button/ButtonHint/ButtonHint.js
@@ -0,0 +1,31 @@
+import './ButtonHint.css';
+import React, { useState } from 'react';
+
+const ButtonHint = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <div>
+            <button className="circle-button" onClick={openModal}>?</button>
+            <Modal isOpen={isModalOpen} onClose={closeModal} />
+        </div>
+        );
+};
+
+const Modal = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <button className="modal-close-button" onClick={onClose}>×</button>
+                <h2>説明書</h2>
+                <p>使い方ここに書く（行によって×ボタンの位置変更いる）</p>
+            </div>
+        </div>
+    );
+};
+
+export default ButtonHint;
