63db97e68db74fde1d0d8294ea796931b6b95171
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-12-06 15:42:56 +0900

    削除機能をコンポーネントのほうに移動した

--- FRONT/src/components/SlidePage/TextBox/TextFontSize.js
+++ FRONT/src/components/SlidePage/TextBox/TextFontSize.js
@@ -1,45 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import TextBox from "./TextBox";
 
-function fontSize (){
-	// フォーカスされたボックスID
-	const [focusedBoxId, setFocusedBoxId] = useState(null);
+function FontSize({
+    selectedBoxId,
+    isTextBoxFocused,
+    setTextBoxes,
+    socket,
+    setSelectedBoxId,
+}) {
+    const increaseFontSize = () => {
+        setTextBoxes((prevTextBoxes) =>
+            prevTextBoxes.map((box, index) =>
+                index === selectedBoxId
+                    ? { ...box, fontSize: box.fontSize + 1 }
+                    : box
+            )
+        );
+    };
 
-	const handleClick = (ref, id) => {
-		if (ref.current) {
-		ref.current.focus(); // クリック時にフォーカスを設定
-		setFocusedBoxId(id); // フォーカスされたボックスの ID を保存
-		}
-	};
+    const decreaseFontSize = () => {
+        setTextBoxes((prevTextBoxes) =>
+            prevTextBoxes.map((box, index) =>
+                index === selectedBoxId
+                    ? { ...box, fontSize: box.fontSize - 1 }
+                    : box
+            )
+        );
+    };
 
-	const increaseFontSize = () => {
-		setFontSize(fontSize + 1); // フォントサイズを1px増加
-	};
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (isTextBoxFocused && selectedBoxId !== null) {
+                if (event.key === "ArrowUp") {
+                    increaseFontSize();
+                } else if (event.key === "ArrowDown") {
+                    decreaseFontSize();
+                }
+            }
+        };
 
-	const decreaseFontSize = () => {
-		setFontSize(fontSize - 1); // フォントサイズを1px減少
-	};
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [isTextBoxFocused, selectedBoxId]);
 
-	//キーボードの↑か↓を押したときに実行するよ
-	const handleKeyDown = (event) => {
-		if (event.key === "↑" && focusedBoxId !== null) {
-			increaseFontSize(); 
-		}
-		if (event.key === "↓" && focusedBoxId !== null) {
-			decreaseFontSize(); 
-		}
-	};
+    return null; // UIを持たないコンポーネントとして機能
+}
 
-	return (
-		<div onKeyDown={handleKeyDown} tabIndex={0}>
-		{/* TextBoxコンポーネントにフォントサイズやクリック処理を渡す */}
-		<TextBox
-			ref={textBoxRef}
-			fontSize={fontSize}
-			onClick={() => handleClick(textBoxRef, 1)} // ボックス1と仮定
-		/>
-		</div>
-	);
-};
-
-export default TextSize;
+export default FontSize;
