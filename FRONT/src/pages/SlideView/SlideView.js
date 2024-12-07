ccb891c51d9916a13ab4fde289b4ccddc08db233
mei <c1419786@st.kanazawa-it.ac.jp>
2024-12-07 12:42:13 +0900

    一時保存

--- FRONT/src/pages/SlideView/SlideView.js
+++ FRONT/src/pages/SlideView/SlideView.js
@@ -2,19 +2,26 @@ import React from "react";
 import "./SlideView.css";
 
 const SlideView = () => {
-const boxCount = 12; 
-return (
-	<div className="SlideView">
-	<div className="leftside">左側</div>
-	<div className="rightside">
-		<div className="box-whole">
-		{Array.from({ length: boxCount }).map((_, index) => (
-			<div className="box" key={index}></div>
-		))}
-		</div>
-	</div>
-	</div>
-);
+  const boxCount = 12;
+  return (
+    <div className="SlideView">
+      <div className="leftside">左側</div>
+      <div className="rightside">
+        <div className="box-whole">
+          {Array.from({ length: boxCount }).map((_, index) => (
+            <div className="box-container" key={index}>
+              <div className="box"></div>
+              <div className="button-group">
+                <button className="button feedback-button">💬</button>
+                <button className="button pin-button">⭐</button>
+                <button className="button create-button">➕</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SlideView;
