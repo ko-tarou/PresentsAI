0a28a5e0de0e1312ff6b7e9cc47f1c0aae59f359
karir <c1406274@st.kanazawa-it.ac.jp>
2024-12-07 09:27:12 +0900

    一時保存

--- FRONT/src/pages/SlideView/SlideView.js
+++ FRONT/src/pages/SlideView/SlideView.js
@@ -5,7 +5,12 @@ const SlideView = () => {
 const boxCount = 12; 
 return (
 	<div className="SlideView">
-	<div className="leftside">左側</div>
+	<div className="leftside">
+		<button className="SlideView-Button1">ボタン1</button>
+		<button className="SlideView-Button2">ボタン2</button>
+		<button className="SlideView-Button3">ボタン3</button>
+		<button className="SlideView-Button4">ボタン4</button>
+	</div>
 	<div className="rightside">
 		<div className="box-whole">
 		{Array.from({ length: boxCount }).map((_, index) => (
