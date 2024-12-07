b05e7e648e129c5a334cb80d75c69ab36d23a690
ko-tarou <tkota6174@gmail.com>
2024-12-07 21:21:50 +0900

    音声追加

--- FRONT/src/pages/audio/AudioRecognition.js
+++ FRONT/src/pages/audio/AudioRecognition.js
@@ -76,7 +76,7 @@ const AudioRecognition = () => {
       const res = await fetch('http://localhost:5000/api/chatgpt', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+        	'Content-Type': 'application/json',
         },
         body: JSON.stringify({ prompt: text }), // 音声認識結果をプロンプトとして送信
       });
