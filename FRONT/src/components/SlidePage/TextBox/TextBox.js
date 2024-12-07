478d3dc77f7b410833ae843fe5543adaf4de6a1d
ko-tarou <tkota6174@gmail.com>
2024-12-08 00:22:01 +0900

    textboxを分割

--- FRONT/src/components/SlidePage/TextBox/TextBox.js
+++ FRONT/src/components/SlidePage/TextBox/TextBox.js
@@ -1,116 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from '../DropZone/ItemTypes.js';
-import ResizeHandle from './ResizeHandle'; // Import the new component
+import ResizeHandle from './ResizeHandle';
+import EditableText from './EditableText.js';
+import { useResizable } from './useResizable.js';
 
 function TextBox({ id, text, x, y, onTextChange, fontSize, onSelect, onFocus, onBlur }) {
-    const [value, setValue] = useState(text || '');
-    const [size, setSize] = useState({ width: 100, height: 50 });
-    const [isEditing, setIsEditing] = useState(false);
+	const { size, handleResizeStart } = useResizable({ width: 100, height: 50 });
+    const [value, setValue] = useState(text);
 
-    useEffect(() => {
-        setValue(text);
-    }, [text]);
+	useEffect(() => {
+		setValue(text);
+	}, [text]);
 
-    const [{ isDragging }, drag] = useDrag({
-        type: ItemTypes.TEXT_BOX,
-        item: { id, x, y },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    });
+	const [{ isDragging }, drag] = useDrag({
+		type: ItemTypes.TEXT_BOX,
+		item: { id, x, y },
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+	});
 
-    const handleChange = (e) => {
-        const newValue = e.target.value;
-        setValue(newValue);
-        onTextChange(id, newValue);
-    };
+	const handleTextChange = (newValue) => {
+		onTextChange(id, newValue);
+	};
 
-    const handleDoubleClick = () => setIsEditing(true);
+	return (
+		<div
+			ref={drag}
+			onClick={() => onSelect && onSelect(id)}
+			onFocus={onFocus}
+			onBlur={onBlur}
+			className='text-box-wrapper'
+			style={{
+				width: `${size.width}px`,
+				height: `${size.height}px`,
+				fontSize: `${fontSize}px`,
+				opacity: isDragging ? 0.5 : 1,
+				padding: '10px',
+				border: '1px solid black',
+				cursor: 'move',
+				position: 'absolute',
+				left: x,
+				top: y,
+				transform: 'translate(-50%, -50%)',
+			}}
+		>
+			<EditableText value={text} onChange={handleTextChange} fontSize={fontSize} />
 
-    const handleBlur = () => setIsEditing(false);
-
-    const handleResizeStart = (e, direction) => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        const startX = e.clientX;
-        const startY = e.clientY;
-        const startWidth = size.width;
-        const startHeight = size.height;
-
-        const handleMouseMove = (e) => {
-            const deltaX = e.clientX - startX;
-            const deltaY = e.clientY - startY;
-
-            setSize({
-                width: direction.includes('right') ? startWidth + deltaX : startWidth - deltaX,
-                height: direction.includes('bottom') ? startHeight + deltaY : startHeight - deltaY,
-            });
-        };
-
-        const handleMouseUp = () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
-        };
-
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp);
-    };
-
-    return (
-        <div
-            ref={drag}
-            onClick={() => onSelect && onSelect(id)}
-            onDoubleClick={handleDoubleClick}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            className='text-box-wrapper'
-            style={{
-                width: `${size.width}px`,
-                height: `${size.height}px`,
-                fontSize: `${fontSize}px`,
-                opacity: isDragging ? 0.5 : 1,
-                padding: '10px',
-                border: '1px solid black',
-                cursor: 'move',
-                position: 'absolute',
-                left: x,
-                top: y,
-                transform: 'translate(-50%, -50%)',
-            }}
-        >
-            {isEditing ? (
-                <input
-                    type="text"
-                    value={value}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    autoFocus
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        border: 'none',
-                        outline: 'none',
-                        fontSize: `${fontSize || 16}px`,
-                    }}
-                />
-            ) : (
-                <div style={{ width: '100%', height: '100%' }}>
-                    {value || 'ダブルクリックで編集'}
-                </div>
-            )}
-
-            {/* リサイズハンドル */}
-            {['top-left', 'top-right', 'bottom-left', 'bottom-right'].map((direction) => (
-                <ResizeHandle
-                    key={direction}
-                    direction={direction}
-                    onResizeStart={handleResizeStart}
-                />
-            ))}
-        </div>
-    );
+			{/* リサイズハンドル */}
+			{['top-left', 'top-right', 'bottom-left', 'bottom-right'].map((direction) => (
+				<ResizeHandle
+					key={direction}
+					direction={direction}
+					onResizeStart={handleResizeStart}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default TextBox;
